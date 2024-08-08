681dd920f1ec861c588f20c11c347edfbc529446
Arshad Beg <arshad.beg@jalantechnologies.com>
2024-08-08 16:29:30 +0530

    Remove shared-task folder from repository

--- src/apps/backend/modules/shared-task/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as ShareTaskServer } from './rest-api/shared-task-server';
-export { default as ShareTaskService } from './shared-task-service';
-export * from './types';
--- src/apps/backend/modules/shared-task/internal/store/shared-task-repository.ts
+++
@@ -1,9 +0,0 @@
-import { ApplicationRepository } from "../../../application";
-import { SharedTaskDB,SharedTaskDBSchema } from "./shared-task-db";
-
-const SharedTaskRepository = ApplicationRepository<SharedTaskDB>(
-    'SharedTask',
-    SharedTaskDBSchema,
-);
-
-export default SharedTaskRepository;
